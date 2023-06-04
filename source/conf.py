fbf429d1826240a5b6a6f066da76e1b813e21b67
Neil Chen <7008563+swc14968@users.noreply.github.com>
2023-06-05 03:13:10 +0800

    Update conf.py
    删除关于 markdown 文档解析的模块引用

--- source/conf.py
+++ source/conf.py
@@ -14,13 +14,9 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = []
 
-source_suffix = {
-	'.rst': 'restructuredtext',
-	'.txt': 'markdown',
-	'.md': 'markdown',
-}
+source_suffix = {}
 
 templates_path = ['_templates']
 exclude_patterns = []
