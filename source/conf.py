8e3f2e20106a206a7b4374811f0a9db0722d5a59
Neil <swc14968@gmail.com>
2023-06-05 00:54:31 +0800

    初始化并尝试将原本的 markdown 版本文件丢入让 sphinx 自动读取解析

---
+++ source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Be a Barista'
+copyright = '2023, Neil'
+author = 'Neil'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+source_suffix = {
+	'.rst': 'restructuredtext',
+	'.txt': 'markdown',
+	'.md': 'markdown',
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
